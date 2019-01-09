a6efae75a8d1a1363ae94ad30b7ab4f48642ee8a
Manuela  Sanchez <m.sanchez947@hotmail.com>
2019-01-09 05:02:38 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -53,9 +53,10 @@ end
 
 def runner
   welcome
-  initial_round
-  until hit?(card_total) > 21
-    puts display_card_total
+  card_total = initial_round
+  until card_total > 21
+    card_total = hit?(card_total)
+    display_card_total
   end_game
 end
 end
